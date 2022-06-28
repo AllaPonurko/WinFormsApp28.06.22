87f22ece128b3a11d0d480a2895eddcc4bfc568f
Alla Ponurko <ponurkoalla264@gmail.com>
2022-06-29 00:57:56 +0300

    111

--- WinFormsApp28.06.22/Form1.Designer.cs
+++ WinFormsApp28.06.22/Form1.Designer.cs
@@ -42,13 +42,20 @@ namespace WinFormsApp28._06._22
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.helpToolStripButton = new System.Windows.Forms.ToolStripButton();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.labeSelect = new System.Windows.Forms.Label();
-            this.radioButtonCircle = new System.Windows.Forms.RadioButton();
-            this.radioButtonRectangle = new System.Windows.Forms.RadioButton();
-            this.radioButtonEllipse = new System.Windows.Forms.RadioButton();
-            this.radioButtonLine = new System.Windows.Forms.RadioButton();
             this.btnConfirm = new System.Windows.Forms.Button();
+            this.radioButtonLine = new System.Windows.Forms.RadioButton();
+            this.radioButtonEllipse = new System.Windows.Forms.RadioButton();
+            this.radioButtonRectangle = new System.Windows.Forms.RadioButton();
+            this.radioButtonCircle = new System.Windows.Forms.RadioButton();
+            this.labeSelect = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labelParams = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.labelLength = new System.Windows.Forms.Label();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.labelRadius = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
             this.toolStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -156,6 +163,13 @@ namespace WinFormsApp28._06._22
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.txtRadius);
+            this.panel1.Controls.Add(this.labelRadius);
+            this.panel1.Controls.Add(this.labelWidth);
+            this.panel1.Controls.Add(this.labelLength);
+            this.panel1.Controls.Add(this.txtWidth);
+            this.panel1.Controls.Add(this.txtLength);
+            this.panel1.Controls.Add(this.labelParams);
             this.panel1.Controls.Add(this.btnConfirm);
             this.panel1.Controls.Add(this.radioButtonLine);
             this.panel1.Controls.Add(this.radioButtonEllipse);
@@ -167,55 +181,15 @@ namespace WinFormsApp28._06._22
             this.panel1.Size = new System.Drawing.Size(200, 368);
             this.panel1.TabIndex = 1;
             // 
-            // panel2
+            // btnConfirm
             // 
-            this.panel2.Location = new System.Drawing.Point(207, 29);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(581, 368);
-            this.panel2.TabIndex = 2;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
-            // 
-            // labeSelect
-            // 
-            this.labeSelect.AutoSize = true;
-            this.labeSelect.Location = new System.Drawing.Point(4, 4);
-            this.labeSelect.Name = "labeSelect";
-            this.labeSelect.Size = new System.Drawing.Size(104, 15);
-            this.labeSelect.TabIndex = 0;
-            this.labeSelect.Text = "Выберите фигуру";
-            // 
-            // radioButtonCircle
-            // 
-            this.radioButtonCircle.AutoSize = true;
-            this.radioButtonCircle.Location = new System.Drawing.Point(4, 33);
-            this.radioButtonCircle.Name = "radioButtonCircle";
-            this.radioButtonCircle.Size = new System.Drawing.Size(55, 19);
-            this.radioButtonCircle.TabIndex = 1;
-            this.radioButtonCircle.TabStop = true;
-            this.radioButtonCircle.Text = "Circle";
-            this.radioButtonCircle.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonRectangle
-            // 
-            this.radioButtonRectangle.AutoSize = true;
-            this.radioButtonRectangle.Location = new System.Drawing.Point(4, 59);
-            this.radioButtonRectangle.Name = "radioButtonRectangle";
-            this.radioButtonRectangle.Size = new System.Drawing.Size(77, 19);
-            this.radioButtonRectangle.TabIndex = 2;
-            this.radioButtonRectangle.TabStop = true;
-            this.radioButtonRectangle.Text = "Rectangle";
-            this.radioButtonRectangle.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonEllipse
-            // 
-            this.radioButtonEllipse.AutoSize = true;
-            this.radioButtonEllipse.Location = new System.Drawing.Point(4, 85);
-            this.radioButtonEllipse.Name = "radioButtonEllipse";
-            this.radioButtonEllipse.Size = new System.Drawing.Size(58, 19);
-            this.radioButtonEllipse.TabIndex = 3;
-            this.radioButtonEllipse.TabStop = true;
-            this.radioButtonEllipse.Text = "Ellipse";
-            this.radioButtonEllipse.UseVisualStyleBackColor = true;
+            this.btnConfirm.Location = new System.Drawing.Point(0, 320);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(104, 23);
+            this.btnConfirm.TabIndex = 5;
+            this.btnConfirm.Text = "Подтвердить";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
             // 
             // radioButtonLine
             // 
@@ -228,15 +202,119 @@ namespace WinFormsApp28._06._22
             this.radioButtonLine.Text = "Line";
             this.radioButtonLine.UseVisualStyleBackColor = true;
             // 
-            // btnConfirm
+            // radioButtonEllipse
             // 
-            this.btnConfirm.Location = new System.Drawing.Point(4, 157);
-            this.btnConfirm.Name = "btnConfirm";
-            this.btnConfirm.Size = new System.Drawing.Size(104, 23);
-            this.btnConfirm.TabIndex = 5;
-            this.btnConfirm.Text = "Подтвердить";
-            this.btnConfirm.UseVisualStyleBackColor = true;
-            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            this.radioButtonEllipse.AutoSize = true;
+            this.radioButtonEllipse.Location = new System.Drawing.Point(4, 85);
+            this.radioButtonEllipse.Name = "radioButtonEllipse";
+            this.radioButtonEllipse.Size = new System.Drawing.Size(58, 19);
+            this.radioButtonEllipse.TabIndex = 3;
+            this.radioButtonEllipse.TabStop = true;
+            this.radioButtonEllipse.Text = "Ellipse";
+            this.radioButtonEllipse.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRectangle
+            // 
+            this.radioButtonRectangle.AutoSize = true;
+            this.radioButtonRectangle.Location = new System.Drawing.Point(4, 59);
+            this.radioButtonRectangle.Name = "radioButtonRectangle";
+            this.radioButtonRectangle.Size = new System.Drawing.Size(77, 19);
+            this.radioButtonRectangle.TabIndex = 2;
+            this.radioButtonRectangle.TabStop = true;
+            this.radioButtonRectangle.Text = "Rectangle";
+            this.radioButtonRectangle.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonCircle
+            // 
+            this.radioButtonCircle.AutoSize = true;
+            this.radioButtonCircle.Location = new System.Drawing.Point(4, 33);
+            this.radioButtonCircle.Name = "radioButtonCircle";
+            this.radioButtonCircle.Size = new System.Drawing.Size(55, 19);
+            this.radioButtonCircle.TabIndex = 1;
+            this.radioButtonCircle.TabStop = true;
+            this.radioButtonCircle.Text = "Circle";
+            this.radioButtonCircle.UseVisualStyleBackColor = true;
+            // 
+            // labeSelect
+            // 
+            this.labeSelect.AutoSize = true;
+            this.labeSelect.Location = new System.Drawing.Point(4, 4);
+            this.labeSelect.Name = "labeSelect";
+            this.labeSelect.Size = new System.Drawing.Size(104, 15);
+            this.labeSelect.TabIndex = 0;
+            this.labeSelect.Text = "Выберите фигуру";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.Location = new System.Drawing.Point(207, 29);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(581, 368);
+            this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // labelParams
+            // 
+            this.labelParams.AutoSize = true;
+            this.labelParams.Location = new System.Drawing.Point(4, 146);
+            this.labelParams.Name = "labelParams";
+            this.labelParams.Size = new System.Drawing.Size(115, 15);
+            this.labelParams.TabIndex = 6;
+            this.labelParams.Text = "Задайте параметры";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Enabled = false;
+            this.txtLength.Location = new System.Drawing.Point(97, 176);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 23);
+            this.txtLength.TabIndex = 7;
+            this.txtLength.TextChanged += new System.EventHandler(this.txtLength_TextChanged);
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Enabled = false;
+            this.txtWidth.Location = new System.Drawing.Point(97, 215);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 23);
+            this.txtWidth.TabIndex = 8;
+            this.txtWidth.TextChanged += new System.EventHandler(this.txtWidth_TextChanged);
+            // 
+            // labelLength
+            // 
+            this.labelLength.AutoSize = true;
+            this.labelLength.Location = new System.Drawing.Point(4, 176);
+            this.labelLength.Name = "labelLength";
+            this.labelLength.Size = new System.Drawing.Size(42, 15);
+            this.labelLength.TabIndex = 9;
+            this.labelLength.Text = "Длина";
+            // 
+            // labelWidth
+            // 
+            this.labelWidth.AutoSize = true;
+            this.labelWidth.Location = new System.Drawing.Point(4, 215);
+            this.labelWidth.Name = "labelWidth";
+            this.labelWidth.Size = new System.Drawing.Size(52, 15);
+            this.labelWidth.TabIndex = 10;
+            this.labelWidth.Text = "Ширина";
+            // 
+            // labelRadius
+            // 
+            this.labelRadius.AutoSize = true;
+            this.labelRadius.Location = new System.Drawing.Point(4, 253);
+            this.labelRadius.Name = "labelRadius";
+            this.labelRadius.Size = new System.Drawing.Size(45, 15);
+            this.labelRadius.TabIndex = 11;
+            this.labelRadius.Text = "Радиус";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Enabled = false;
+            this.txtRadius.Location = new System.Drawing.Point(97, 250);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 23);
+            this.txtRadius.TabIndex = 12;
+            this.txtRadius.TextChanged += new System.EventHandler(this.txtRadius_TextChanged);
             // 
             // Form1
             // 
@@ -279,6 +357,13 @@ namespace WinFormsApp28._06._22
         private System.Windows.Forms.RadioButton radioButtonRectangle;
         private System.Windows.Forms.RadioButton radioButtonCircle;
         private System.Windows.Forms.Label labeSelect;
+        private System.Windows.Forms.Label labelWidth;
+        private System.Windows.Forms.Label labelLength;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label labelParams;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Label labelRadius;
     }
 }
 
--- WinFormsApp28.06.22/Form1.cs
+++ WinFormsApp28.06.22/Form1.cs
@@ -19,18 +19,65 @@ namespace WinFormsApp28._06._22
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            if (radioButtonCircle.Checked)
+                txtRadius.Enabled = true;
+            if (radioButtonEllipse.Checked|| radioButtonRectangle.Checked)
+            {
+                txtLength.Enabled = true;
+                txtWidth.Enabled = true;
+            }
         }
 
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-
+            SolidBrush redBrush = new SolidBrush(Color.Red);
         }
 
         private void btnConfirm_Click(object sender, EventArgs e)
         {
+              if(radioButtonCircle.Checked)
+            { 
+                Pen pen = new Pen(Color.Red, 3);
+                Rectangle rectangle = new Rectangle();
+                panel2.CreateGraphics().DrawEllipse(pen, rectangle);
+            }
+              if(radioButtonEllipse.Checked)
+            {
+                
+                Pen pen = new Pen(Color.Red, 3);
+                Rectangle rectangle = new Rectangle();
+                panel2.CreateGraphics().DrawEllipse(pen, rectangle);
+            }
+              if(radioButtonRectangle.Checked)
+            {
+                
+                Pen pen = new Pen(Color.Red, 3);
+                Rectangle rectangle = new Rectangle();
+                panel2.CreateGraphics().DrawRectangle(pen, rectangle);
+            }
+             if(radioButtonLine.Checked)
+            {
+                Pen pen = new Pen(Color.Red, 3);
+                Point p1 = new Point(1,69);
+                Point p2 = new Point(26,98);
+                panel2.CreateGraphics().DrawLine(pen,p1,p2);
+            }
+        }
 
+        private void txtLength_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void txtWidth_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtRadius_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
-}
+    }
+
